49bbf0e0701ba0879e7db54d08f2de6e5494b058
Kshitij <97899670+KshitijGupta4832@users.noreply.github.com>
2022-10-20 13:47:34 +0530

    Add files via upload
    Digital clock

---
+++ Clock.js
@@ -0,0 +1,23 @@
+setInterval(Time, 1000)
+function Time() {
+    let date = new Date()
+    let hh = date.getHours()
+    let mm = date.getMinutes()
+    let ss = date.getSeconds()
+    let period = "AM"//Default
+    
+    if(hh>00&&hh>12){
+        hh = hh-12
+        period = "PM"
+    }
+    // console.log(hh,mm,ss)
+    let current = `${hh} - ${mm} - ${ss} ${period} `
+    
+
+    document.getElementById("Time").innerHTML = current
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let day = date.getDay()
+    document.getElementById("day").innerHTML = days[day]
+}
+Time()
+
